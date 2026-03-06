28422cfb8d63071b2b6bcd8be202259b966d3ad9
Eli McDonald <mcd0018@humecentralsc.vic.edu.au>
2026-03-06 00:19:16 -0800

    Hopefully final fix
    I think outdated certs are killing my connection to that; imma try using http instead

--- script.js
+++ script.js
@@ -15,78 +15,27 @@
 
     if (window.location.pathname.indexOf('recipeviewer.html') !== -1) {
         var container = document.getElementById('recipetext');
-        var linkSpan = document.getElementById('original-link');
         var u = "";
-        
         var query = window.location.search.substring(1).split('&');
         for (var i = 0; i < query.length; i++) {
             var pair = query[i].split('=');
-            if (pair[0] === 'url') {
-                u = decodeURIComponent(pair[1]);
-            }
+            if (pair[0] === 'url') u = decodeURIComponent(pair[1]);
         }
-        
         if (!u) u = localStorage.getItem('lastURL');
 
         if (u) {
-            if (linkSpan) linkSpan.innerHTML = '<a href="' + u + '">' + u + '</a>';
-            container.innerText = "LOADING VIA SERVER PROXY...";
-            
+            container.innerText = "ATTEMPTING INSECURE PROXY FETCH...";
             var x = new XMLHttpRequest();
-            var proxyUrl = "https://corsproxy.io/?" + encodeURIComponent(u);
+            var proxy = "http://api.allorigins.win/get?url=" + encodeURIComponent(u);
             
-            x.open('GET', proxyUrl, true);
+            x.open('GET', proxy, true);
             x.onreadystatechange = function() {
-                if (x.readyState === 4 && x.status === 200) {
-                    var html = x.responseText;
-                    var jsonM = html.match(/<script [^>]*type=["']application\/ld\+json["'][^>]*>([\s\S]*?)<\/script>/gi);
-                    var recipe = null;
-                    
-                    if (jsonM) {
-                        for (var j = 0; j < jsonM.length; j++) {
-                            try {
-                                var cleanJson = jsonM[j].replace(/<script.*?>/gi, '').replace(/<\/script>/gi, '').replace(/^\s+|\s+$/g, '');
-                                if (cleanJson.charAt(cleanJson.length - 1) === ';') cleanJson = cleanJson.slice(0, -1);
-                                var data = JSON.parse(cleanJson);
-                                
-                                if (data['@graph']) {
-                                    for (var g = 0; g < data['@graph'].length; g++) {
-                                        if (data['@graph'][g]['@type'] === 'Recipe') {
-                                            recipe = data['@graph'][g];
-                                            break;
-                                        }
-                                    }
-                                } else if (data['@type'] === 'Recipe' || (data['@type'] && data['@type'].indexOf('Recipe') !== -1)) {
-                                    recipe = data;
-                                }
-                                if (recipe) break;
-                            } catch (e) {}
-                        }
-                    }
-
-                    if (recipe) {
-                        var out = (recipe.name || "RECIPE").toUpperCase() + "\n\nINGREDIENTS:\n";
-                        var ing = recipe.recipeIngredient || [];
-                        for (var k = 0; k < ing.length; k++) {
-                            out += "• " + ing[k] + "\n";
-                        }
-                        out += "\nINSTRUCTIONS:\n";
-                        var steps = recipe.recipeInstructions || [];
-                        if (!Array.isArray(steps)) steps = [steps];
-                        for (var l = 0; l < steps.length; l++) {
-                            var val = (typeof steps[l] === 'string') ? steps[l] : (steps[l].text || "");
-                            if (val) out += (l + 1) + ". " + val + "\n\n";
-                        }
-                        
-                        var notesM = html.match(/id="recipe[^"]*notes"[^>]*>([\s\S]*?)<\/div>/i);
-                        if (notesM && notesM[1]) {
-                            out += "NOTES:\n" + notesM[1].replace(/<[^>]*>?/gm, '').replace(/\s+/g, ' ');
-                        }
-
-                        container.innerText = out;
-                        localStorage.setItem('lastRecipe', out);
+                if (x.readyState === 4) {
+                    if (x.status === 200) {
+                        var d = JSON.parse(x.responseText).contents;
+                        processRecipe(d);
                     } else {
-                        container.innerText = "SERVER RETURNED DATA BUT NO RECIPE FOUND.";
+                        container.innerText = "SSL/CONNECTION ERROR: YOUR 4S CANNOT REACH THE PROXY.";
                     }
                 }
             };
@@ -95,17 +44,46 @@
     }
 })();
 
+function processRecipe(html) {
+    var container = document.getElementById('recipetext');
+    var jsonM = html.match(/<script [^>]*type=["']application\/ld\+json["'][^>]*>([\s\S]*?)<\/script>/gi);
+    var r = null;
+    if (jsonM) {
+        for (var j = 0; j < jsonM.length; j++) {
+            try {
+                var s = jsonM[j].replace(/<script.*?>/gi, '').replace(/<\/script>/gi, '').replace(/^\s+|\s+$/g, '');
+                if (s.charAt(s.length - 1) === ';') s = s.slice(0, -1);
+                var data = JSON.parse(s);
+                if (data['@graph']) {
+                    for (var g = 0; g < data['@graph'].length; g++) {
+                        if (data['@graph'][g]['@type'] === 'Recipe') { r = data['@graph'][g]; break; }
+                    }
+                } else if (data['@type'] === 'Recipe') { r = data; }
+                if (r) break;
+            } catch (e) {}
+        }
+    }
+    if (r) {
+        var out = (r.name || "RECIPE").toUpperCase() + "\n\nINGREDIENTS:\n";
+        for (var k = 0; k < r.recipeIngredient.length; k++) out += "• " + r.recipeIngredient[k] + "\n";
+        out += "\nINSTRUCTIONS:\n";
+        var steps = r.recipeInstructions;
+        if (!Array.isArray(steps)) steps = [steps];
+        for (var l = 0; l < steps.length; l++) {
+            var v = (typeof steps[l] === 'string') ? steps[l] : (steps[l].text || "");
+            out += (l + 1) + ". " + v + "\n\n";
+        }
+        container.innerText = out;
+        localStorage.setItem('lastRecipe', out);
+    }
+}
+
 var btn = document.getElementById('recipesubmit');
 if (btn) {
     btn.onclick = function() {
-        var input = document.getElementById('recipeurl').value.replace(/^\s+|\s+$/g, '');
-        if (input.indexOf('http') === 0) {
-            localStorage.setItem('lastURL', input);
-            window.location.href = 'recipeviewer.html?url=' + encodeURIComponent(input);
-        } else {
-            localStorage.setItem('lastRecipe', input);
-            window.location.href = 'recipeviewer.html';
-        }
+        var val = document.getElementById('recipeurl').value.replace(/^\s+|\s+$/g, '');
+        localStorage.setItem('lastURL', val);
+        window.location.href = 'recipeviewer.html?url=' + encodeURIComponent(val);
         return false;
     };
 }
